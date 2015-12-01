46fd3486365e598dcaa0e4a456471303ec619bf0
Felipe Guerra <administrador@vidasaudavelorganicos.com.br>
2015-12-01 09:17:36 -0200

    Fix suggested by Diego

--- app/route/index.js
+++ app/route/index.js
@@ -6,43 +6,35 @@ const Boom = require('boom');
 
 var routes = [];
 
-var schema = Joi.object().keys({
-	points: Joi.object().keys({
-		from: Joi.object().keys({
-			lat: Joi.number().min(-180).max(180).required(),
-			lgt: Joi.number().min(-90).max(90).required(),
-		}),
-		to: Joi.object().keys({
-			lat: Joi.number().min(-180).max(180).required(),
-			lgt: Joi.number().min(-90).max(90).required(),
-		})		
-	})
-});
-
 routes.push({
-	method: 'POST',
-	path: '/geolocation/calculate-distance-between-two-points',
-	handler: function (request, response) {
-        var jSon = JSON.parse(request.payload);
-
-        Joi.validate(jSon, schema, function(err, value) {
-        	if(err) {
-        		console.error('Invalid request', err);
-        		return response(Boom.create(400, 'Bad Request', {message: err.name}));
-
-        	}else {
-	       		logic.calculateDistanceBetweenTwoPoints(jSon.points, function(result, err) {
-					if(err) {
-						return response(Boom.create(500, 'Error on calculate distance.', 
-													{ timestamp: Date.now() }));
-					}else {
-						return response(result);
-					}
-				});
-       		}
+    method: 'POST',
+    path: '/geolocation/calculate-distance-between-two-points',
+    config: {
+        validate: {
+            payload: Joi.object().keys({
+                points: Joi.object().keys({
+                    from: Joi.object().keys({
+                        lat: Joi.number().min(-180).max(180).required(),
+                        lgt: Joi.number().min(-90).max(90).required(),
+                    }),
+                    to: Joi.object().keys({
+                        lat: Joi.number().min(-180).max(180).required(),
+                        lgt: Joi.number().min(-90).max(90).required(),
+                    })
+                })
+            })
+        }
+    },
+    handler: function (request, response) {
+        logic.calculateDistanceBetweenTwoPoints(request.payload.points, function(result, err) {
+            if(err) {
+                return response(Boom.create(500, 'Error on calculate distance.', 
+                                            { timestamp: Date.now() }));
+            }else {
+                return response(result);
+            }
         });
-        
-	}
+    }
 });
 
 module.exports = routes;
